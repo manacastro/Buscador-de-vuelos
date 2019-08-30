cecd845302d57f6fac4defafde871200129469b8
Mariana Castro <mb.castro17@gmail.com>
2019-08-30 13:03:48 -0300

    lenguaje y monedas

---
+++ adatpfinal/src/components/Currencies/Currencies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Currencies.scss';
+
+const Currencies = (exchange) => (
+    <div className='ContentCurrencies'>
+        <p className='codeCurrencies'>{exchange.exchange.code} </p>
+        <p className='TitleCurrencies'>{exchange.exchange.title}</p>
+    </div>
+)
+
+export default Currencies
