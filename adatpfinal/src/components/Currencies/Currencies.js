b1aca001c335418a4f83c4ab28cc142a4458090a
Mariana Castro <mb.castro17@gmail.com>
2019-08-30 13:09:05 -0300

    lenguaje y monedas

--- adatpfinal/src/components/Currencies/Currencies.js
+++ adatpfinal/src/components/Currencies/Currencies.js
@@ -3,6 +3,7 @@ import './Currencies.scss';
 
 const Currencies = (exchange) => (
     <div className='ContentCurrencies'>
+        
         <p className='codeCurrencies'>{exchange.exchange.code} </p>
         <p className='TitleCurrencies'>{exchange.exchange.title}</p>
     </div>
